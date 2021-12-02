ee52109f409084a90833e8c00c577bd8afd154ae
2235g3 <60404662+2235g3@users.noreply.github.com>
2021-12-02 10:55:42 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,15 +7,26 @@ public class Main {
     public static void main(String[] args) {
         String compGenNum = Integer.toString(randGen()) + randGen() + randGen() + randGen();
         String userNum;
+	int amtInRightPlace = 0;
         System.out.println(compGenNum);
-        while (true) {
-            System.out.println("Enter a 4 digit number:");
-            userNum = input();
-            if (userNum != "-1") {
-                break;
-            }
-        }
-        numCompare(compGenNum, userNum);
+	for (int i = 12; i > 0; i--) {
+		System.out.println("You have " + i + " guesses left");
+        	while (true) { 
+            		System.out.println("Enter a 4 digit number:");
+            		userNum = input();
+            		if (userNum != "-1") {
+                		break;
+            		}
+        	}
+        	amtInRightPlace = numCompare(compGenNum, userNum);
+		if (amtInRightPlace == 4) {
+			System.out.println("You got all digits correct!\nYou had " + (i - 1) + " guesses left");
+			break;
+		}
+	}
+	if (amtInRightPlace < 4) {
+		System.out.println("You lost!\nThe computer's number was " + compGenNum);
+	}
     }
     public static String input() {
         Scanner input = new Scanner(System.in);
@@ -36,12 +47,14 @@ public class Main {
     }
     public static int randGen() {
         Random rand = new Random();
-        return rand.nextInt(10);
+        return (rand.nextInt(9-1+1) + 1);
     }
-    public static void numCompare(String compGenNum, String userNum) {
+    public static int numCompare(String compGenNum, String userNum) {
+	int amtInRightPlace = 0;
         for (int i = 0; i < 4; i++) {
             if (compGenNum.charAt(i) == userNum.charAt(i)) {
                 System.out.println("Digit " + (i + 1) + " is in the right place");
+		amtInRightPlace += 1;
             }
             else if (compGenNum.contains(String.valueOf(userNum.charAt(i)))) {
                 System.out.println("Digit " + (i + 1) + " is not in the right position but is in the computer's number");
@@ -50,5 +63,7 @@ public class Main {
                 System.out.println("Digit " + (i + 1) + " is not in the right place");
             }
         }
+    System.out.println("\n");
+	return amtInRightPlace;
     }
 }
